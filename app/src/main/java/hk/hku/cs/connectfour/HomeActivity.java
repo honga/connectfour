031f0d9b973378146dbc4ace767e7e4d074601bd
Hong <hng@demystdata.com>
2016-10-10 23:38:43 +0800

    initial commit

---
+++ app/src/main/java/hk/hku/cs/connectfour/HomeActivity.java
@@ -0,0 +1,13 @@
+package hk.hku.cs.connectfour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
