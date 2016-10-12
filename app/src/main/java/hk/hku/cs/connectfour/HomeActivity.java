c7c781af3b7031493f89dcee6cc0d1f0b60b38fc
Hong <hng@demystdata.com>
2016-10-12 23:52:44 +0800

    get button to work

--- app/src/main/java/hk/hku/cs/connectfour/HomeActivity.java
+++ app/src/main/java/hk/hku/cs/connectfour/HomeActivity.java
@@ -2,12 +2,30 @@ package hk.hku.cs.connectfour;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button start_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        start_button = (Button)findViewById(R.id.start_button);
+
+        start_button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if (v.getId() == R.id.start_button){
+            // do sth
+            System.out.println("################\n" +
+                                "Hey Ho let's go!\n" +
+                                "################");
+        }
     }
 }
