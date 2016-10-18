b9d338e60c5892b994cd9282388c1303111f5f3e
Hong <hng@demystdata.com>
2016-10-19 01:25:00 +0800

    can start new game and retract moves now

--- app/src/main/java/hk/hku/cs/connectfour/GameActivity.java
+++ app/src/main/java/hk/hku/cs/connectfour/GameActivity.java
@@ -27,6 +27,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     TableLayout board;
     String TURN = "RED";
     String[][] gameState = new String[6][7];
+    int[][] drawBoard = new int[6][7];
+    ArrayList<int[]> historical = new ArrayList<int[]>();
 
 
     @Override
@@ -38,15 +40,16 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         retract = (Button)findViewById(R.id.retract);
         board = (TableLayout) findViewById(R.id.board);
         board.setShrinkAllColumns(true);
-        LinearLayout l;
-        TableRow r;
+
 
         //set OnClickListener for the entire board
-        for (int i=0;i<board.getChildCount();i++){
-            l = (LinearLayout) board.getChildAt(i);
-            for (int j=0;j<l.getChildCount();j++){
-                r = (TableRow) l.getChildAt(j);
-                for (int k=0;k<r.getChildCount();k++){
+        // add each ImageView ID to corresponding 2D array location
+        for (int i=0; i<board.getChildCount(); i++){
+            LinearLayout l = (LinearLayout) board.getChildAt(board.getChildCount()-1-i);
+            for (int j=0; j<l.getChildCount(); j++){
+                TableRow r = (TableRow) l.getChildAt(j);
+                for (int k=0; k<r.getChildCount(); k++){
+                    drawBoard[i][k] = r.getChildAt(k).getId();
                     r.getChildAt(k).setOnClickListener(this);
                 }
             }
@@ -59,23 +62,19 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
     public void onClick(View v){
         if (v.getId() == R.id.new_game){
-            // do sth
-            System.out.println("################\n" +
-                                "AGAIN AGAIN!\n" +
-                                "################");
+            gameState = new String[6][7];
+            historical = new ArrayList<int[]>();
+            drawBoard(gameState);
         }
-
-        if (v.getId() == R.id.retract){
-            // retract moves nonstop
-            System.out.println("$$$$$$$$$$$$\n" +
-                                "GG NO RE\n" +
-                                "$$$$$$$$$$$$\n");
-        }
-
         if (v instanceof ImageView){
             String playerMove = v.getResources().getResourceName(v.getId());
             int col = Character.getNumericValue(playerMove.charAt(playerMove.length()-1));
             int row = Character.getNumericValue(playerMove.charAt(playerMove.length()-2));
+            int[] newMove = new int[2];
+            newMove[0] = row;
+            newMove[1] = col;
+            historical.add(newMove);
+
             if (TURN == "RED"){
                 ImageView redMove;
                 System.out.println("ZOMG!\n");
@@ -83,6 +82,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                 System.out.println("what col did we press!?  " + col);
                 System.out.println("WHOSE TURN: " + TURN);
                 System.out.println("####################\n\n");
+                gameState[row][col] = "r";
                 System.out.println(Arrays.deepToString(gameState));
                 System.out.println("####################\n\n");
                 // TODO make new method
@@ -91,11 +91,46 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                 TURN = "GREEN";
             } else if (TURN == "GREEN"){
                 ImageView greenMove;
+                gameState[row][col] = "g";
                 greenMove = (ImageView) findViewById(v.getId());
                 greenMove.setImageResource(R.drawable.green_t);
                 System.out.println("WHOSE TURN: " + TURN);
                 TURN = "RED";
             }
         }
+        if (v.getId() == R.id.retract && historical.size() > 0){
+            retractMove(gameState, historical);
+            drawBoard(gameState);
+            System.out.println("game state after retract is: " + Arrays.deepToString(gameState));
+            System.out.println("drawBoard after retract is: " + Arrays.deepToString(drawBoard));
+            System.out.println("game state after draw is: " + Arrays.deepToString(gameState));
+        }
+    }
+
+    public void drawBoard(String[][] gameState){
+        for (int j=0; j<gameState.length; j++){
+            for(int k=0; k<gameState[j].length; k++){
+                ImageView setMove = (ImageView) findViewById(drawBoard[j][k]);
+                setMove.setImageResource(R.drawable.empty_t);
+                if (gameState[j][k] == "r"){
+                    setMove = (ImageView) findViewById(drawBoard[j][k]);
+                    setMove.setImageResource(R.drawable.red_t);
+                } else if (gameState[j][k] == "g"){
+                    setMove = (ImageView) findViewById(drawBoard[j][k]);
+                    setMove.setImageResource(R.drawable.green_t);
+
+                }
+            }
+        }
+    }
+
+    public void checkWin(String [][] gameState, ImageView[][] drawBoard){
+        // if win change the image of winning pieces
+    }
+
+    public void retractMove(String[][] gameState, ArrayList<int[]> historical){
+        //retract move
+        int[] move = historical.remove(historical.size() - 1);
+        gameState[move[0]][move[1]] = null;
     }
 }
